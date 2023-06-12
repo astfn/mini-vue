1b9579ee6285f96e6b04c783510b57123854ffd6
astfn <876458652@qq.com>
2023-06-12 22:47:50 +0800

    chore: 双端对比整体框架梳理；example代码打桩

--- 项目/mini-vue/example/updateElementChildren/ArrayToArray.js
+++ 项目/mini-vue/example/updateElementChildren/ArrayToArray.js
@@ -1,5 +1,31 @@
 import { useState } from "./hooks.js";
 
+/**
+ * 1.自左向右对比，找到不同处，指针停止移动。
+ *   此时右半部分恰好就是要处理的区间
+ *   并且只需新增或删除节点
+ */
+////1.1 new children 比 old children 长 (新增节点)
+const oldArray = ["A", "B"];
+const newArray = ["A", "B", "D", "E"];
+
+//// 1.2 new children 比 old children 短 (删除节点)
+// const oldArray = ["A", "B", "D", "E"];
+// const newArray = ["A", "B"];
+
+/**
+ * 2.自右向左对比，找到不同处，指针停止移动。
+ *   此时左半部分恰好就是要处理的区间
+ *   并且只需新增或删除节点
+ */
+////2.1 new children 比 old children 长 (新增节点)
+// const oldArray = ["B", "C"];
+// const newArray = ["D", "E", "B", "C"];
+
+//// 2.2 new children 比 old children 短 (删除节点)
+// const oldArray = ["D", "E", "B", "C"];
+// const newArray = ["B", "C"];
+
 export default {
   setup() {
     const [convert, setConvert] = useState(false);
@@ -10,10 +36,8 @@ export default {
     };
   },
   render(h) {
-    const oldArray = [1, 2, 3];
-    const newArray = [2, 3, 4];
     const targetChildren = (this.convert ? newArray : oldArray).map((item) =>
-      h("li", {}, item)
+      h("li", { key: item }, item)
     );
     return h("div", {}, [
       h("button", { onClick: () => this.setConvert(true) }, "ArrayToArray"),
